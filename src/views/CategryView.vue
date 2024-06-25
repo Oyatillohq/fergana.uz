316037d96197cd90482b7aceb2b7e3e9eef8fed8
oyatillohq <qurbonaliyev.com@gmail.com>
2024-06-25 16:17:57 +0500

    category add

--- src/views/CategryView.vue
+++ src/views/CategryView.vue
@@ -1,23 +1,24 @@
 <template>
-    <div style="display: grid; grid-template-columns: repeat(2, 1fr) ">
-        <div class="container">
-            <div class="Uzbekistan-card" style="width: 310px; display:flex " v-for="item in detail?.data?.news_list "
-                :key="item">
+    <div style="display: grid; margin-top: 50px
+     /* grid-template-columns: repeat(2, 1fr) */
+     "> 
+        <div class="container" style="display:flex; gap: 50px;">
+            <div class="Uzbekistan-card" style="width: 310px; " v-for="item in detail?.data?.news_list " :key="item">
                 <img class="prisdent-card-img" :src="`${store.baseUrl}` + item.image">
-                <h2>{{ baseUrl }}</h2>
-                <router-link to="/" class="Uz-card-info">{{ item.category_id }}</router-link>
+                <router-link :to="`news/${item.slug}`" class="Uz-card-info">{{ item.category_id }}</router-link>
                 <div class="Uzbekistan-card__text-wrapper">
 
-                    <router-link to="/" class="Uz-desc">
+                    <router-link :to="`item/${item.slug}`" class="Uz-desc">
+                        {{ item.title }}
                     </router-link>
                     <div class="time-info">
                         <div class="time-wrap">
                             <img src="@/assets/images/png/clock.png" alt="">
-                            <h2>11:00 / 13.10.2021</h2>
+                            <h2>{{ item.date }}</h2>
                         </div>
                         <div class="time-wrap">
                             <img src="@/assets/images/png/ease.png" alt="">
-                            <h2>14550</h2>
+                            <h2>{{ item.count_view }}</h2>
                         </div>
                     </div>
                 </div>
@@ -42,4 +43,11 @@ getDetailCategory()
 console.log(detail);
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+    width: 100%;
+    max-width: 1380px;
+    margin: 0 auto;
+    padding: 0 20px;
+}
+</style>
--- src/views/NewsDetail.vue
+++ src/views/NewsDetail.vue
@@ -2,25 +2,29 @@
     <div class="news-detail">
         <div class="container">
 
-            
-        <div class="data" style="width: 310px;">
-                <div class="news-detail__card" v-for="item in detail?.data?.last_news" :key="item" >
+            <div v-html="detail?.data?.news_detail.content" class="news-detail__html"></div>
+            <div class="data" style="width: 310px;">
+                <div class="rec">
+                    <div class="rec-t"></div>Tavsiya etamiz
+                </div>
+                <router-link :to="`/news/${item.slug}`" class="news-detail__card"
+                    v-for="item in detail?.data?.last_news" :key="item">
                     <h3 class="title">{{ item.category_id }}</h3>
                     <img :src="item.image" alt="">
-                    <router-link to="`/`" class="Uz-desc">
+                    <router-link to="/" class="Uz-desc">
                         {{ item.title }}
                     </router-link>
                     <div class="time-info">
                         <div class="time-wrap">
                             <img src="@/assets/images/png/clock.png" alt="">
-                            <h2>{{item.date}}</h2>
+                            <h2>{{ item.date }}</h2>
                         </div>
                         <div class="time-wrap">
                             <img src="@/assets/images/png/ease.png" alt="">
                             <h2>{{ item.count_view }}</h2>
                         </div>
                     </div>
-                </div>
+                </router-link>
             </div>
         </div>
     </div>
@@ -31,13 +35,21 @@ import { ref } from 'vue';
 import services from '@/services/services';
 import { useRoute } from 'vue-router';
 const route = useRoute()
-const detail = ref({})
+const detail = ref({});
 const getDetail = async () => {
     const res = await services.getNewsDetail(route.params.slug)
     detail.value = res.data
-}
+};
 getDetail();
-console.log(detail?.data?.news_detail);
+import { useStore } from '@/store/store';
+const store = useStore()
+async function getDetailCategory() {
+    const res = await services.getDetailCategory(route.params.id, route.params.slug)
+    detail.value = res.data;
+    console.log(detail.data.last_news);
+}
+getDetailCategory()
+console.log(detail);
 </script>
 
 <style lang="scss" scoped>
@@ -45,7 +57,8 @@ console.log(detail?.data?.news_detail);
     display: flex;
     flex-direction: column;
     max-width: 380px;
-    .title{
+
+    .title {
         color: #DFAD2A;
     }
 }
@@ -62,8 +75,6 @@ console.log(detail?.data?.news_detail);
     margin: 0 auto;
     padding: 0 20px;
     display: flex;
-    flex-direction: row;
-    gap: 50px;
 }
 
 .text-wrapper {
@@ -72,12 +83,52 @@ console.log(detail?.data?.news_detail);
 }
 
 .news-detail__html {
-
+    width: 990px;
+    padding-top: 50px;
     * {
         font-family: Arial, Helvetica, sans-serif;
         display: flex;
         flex-direction: column;
         justify-content: center;
+        text-align: left;
     }
+}
+
+
+.data {
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+    margin-top: 50px;
+
+    .rec {
+        font-size: 20px;
+        font-weight: 700;
+        line-height: 23px;
+        margin-bottom: -20px;
+        display: flex;
+        align-items: center;
+        justify-content: left;
+
+        &-t {
+            width: 15px;
+            height: 15px;
+            background: #DFAD2A;
+            border-radius: 50%;
+        }
+
+    }
+
+    .news-detail__card {
+        padding: 10px;
+        border: 1px solid #c9c9c9;
+
+        &:hover {
+            border: 2px solid #DFAD2A;
+            cursor: pointer;
+        }
+
+    }
+
 }
 </style>
